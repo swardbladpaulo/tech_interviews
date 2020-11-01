48817fce4651be8c8772e6a44573353c4f85e130
swardbladpaulo <paulo@swardblads.se>
2020-11-01 09:56:30 +0100

    modified the instance_double, test passed

--- cars/car.rb
+++ cars/car.rb
@@ -1,9 +1,19 @@
 class Car
-  attr_accessor :color
+
+  attr_accessor :color, :driver
+
   def initialize()
-     @color = 'silver'    
+    @color = 'silver'
+    @driver = 'Paulo'    
   end
+
   def new_paint(newcolor)
       @color = newcolor
   end
+
+  def car_driver(driver)
+    @driver = driver
+  end
+
+
 end
--- spec/cars_spec.rb
+++ spec/cars_spec.rb
@@ -1,11 +1,19 @@
 require './cars/car.rb'
 
 describe Car do
+    let (:driver) {instance_double('driver', name: 'Paulo')}
+
     it 'has the color silver on initialize' do
         expect(subject.color).to eq 'silver'
     end
+
     it 'can change color' do
         subject.new_paint('brown')
         expect(subject.color).to eq 'brown'
+    end
+
+    it 'has a driver' do
+        subject.car_driver('Paulo')
+        expect(subject.driver).to eq 'Paulo'
     end
 end
